8b4a121e135aff2521f98c08c036648a73ec87a9
Nicole0509 <niashavril0509@gmail.com>
2025-09-17 14:18:22 +0200

    Invokes the Triangle features using an object

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,13 @@ public class Main {
 
         Circle circle = new Circle( 3);
         Rectangle rectangle = new Rectangle(4 ,5);
-
+        Triangle triangle = new Triangle(2, 3);
 
         System.out.printf("The area of the circle is %.2f cm²\n" , circle.calculateArea());
 
         System.out.printf("The area of the rectangle is %.2f cm²\n" , rectangle.calculateArea());
+
+        System.out.printf("The area of the rectangle is %.2f cm²" , triangle.calculateArea());
 
     }
 }

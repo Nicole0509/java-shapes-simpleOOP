fe2ef9d64b8d641e93feddf53b0be7d6e55bc1bc
Nicole0509 <niashavril0509@gmail.com>
2025-09-17 12:44:57 +0200

    Invoked the calculate area using the Circle object

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,12 @@ public class Main {
         System.out.println("Welcome To Shapes OOP!");
 
         Circle circle = new Circle( 3);
+        Rectangle rectangle = new Rectangle(4 ,5);
 
-        System.out.printf("The area of the circle is %.2f cm²" , circle.calculateArea());
+
+        System.out.printf("The area of the circle is %.2f cm²\n" , circle.calculateArea());
+
+        System.out.printf("The area of the rectangle is %.2f cm²\n" , rectangle.calculateArea());
 
     }
 }
